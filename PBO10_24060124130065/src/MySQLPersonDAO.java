ceaeb18218acf5ff98647f95741eabb18dc43766
Elza K Carrynda <elzakcarrynda@gmail.com>
2026-05-12 14:56:50 +0700

    pertemuan10

---
+++ PBO10_24060124130065/src/MySQLPersonDAO.java
@@ -0,0 +1,18 @@
+import java.sql .*;
+public class MySQLPersonDAO implements PersonDAO{
+    public void savePerson (Person person) throws Exception{
+        String name = person.getName() ;
+        //membuat koneksi, nama db, user, password menyesuaikan
+        Class.forName("com.mysql.cj.jdbc.Driver") ;
+        Connection con = DriverManager.getConnection (
+                        "jdbc:mysql://127.0.0.1/pbo", "root","shinsukekita");
+        //kerjakan mysql query
+        String query = "INSERT INTO person (name) VALUES ('"+name+"')";
+        System.out.println (query) ;
+        Statement s = con.createStatement ();
+        s.executeUpdate (query) ;
+        //tutup koneksi database
+        con.close();
+    }
+
+}
